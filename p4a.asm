84d5de6d9da3284fcfa8acc22556e2004a2d4b26
Adrián Palmero <adripalmeromartinez@gmail.com>
2020-05-03 06:16:57 +0200

    install/uninstall arguments

--- p4a.asm
+++ p4a.asm
@@ -39,16 +39,16 @@ desinstalar:
             PUSH AX BX CX DS ES
 
             MOV CX, 0
-            MOV DS, CX
-            MOV ES, DS:[60H*4+2]
-            MOV BX, ES:[2CH]
+            MOV DS, CX              ; Segmento de vectores interrupcion
+            MOV ES, DS:[60H*4+2]    ; Lee segmento RSI
+            MOV BX, ES:[2CH]        ; Lee segmento de entorno del PSP de RSI
 
             MOV AH, 49H
-            INT 21H
+            INT 21H                 ; Libera segmento de RSI (es)
             MOV ES, BX
-            INT 21H
+            INT 21H                 ; Libera segmento de variables de entorno de RSI
 
-            CLI
+            CLI                     ; Pone a cero vecto de interrumpción 40h
             MOV DS:[60H*4], CX
             MOV DS:[60H*4+2], CX
             STI
@@ -58,13 +58,45 @@ desinstalar:
         desinstalar_60h ENDP
 
 
-rutina_interrumpcion:
+rutina_interrupcion:
         rutina_int60 PROC FAR
 
         rutina_int60 ENDP
 
-main: ;Código principal
+main:   ; Código principal
 
+        ; Guarda los dos primeros caracteres de los argumentos en el registro AX
+        ; (orden inverso)
+        MOV AX, DS:[82H] ; Acceso al PSP
+
+        ; Argumentos:   /D => AX = 442F
+        ;               /I => AX = 492F
+
+        CMP AX, 442FH
+        JE argumento_d
+
+        CMP AX, 492FH
+        JE argumento_i
+
+        ; Si no existen los argumentos /D ni /I
+        JMP no_args
+
+argumento_d:
+        ; Ejecucion con /D
+        CALL desinstalar_60h
+        JMP fin
+
+argumento_i:
+        ; Ejecucion con /I
+        CALL instalar_60h
+        ; La funcion instalar termina el programa con la interrupcion INT 27H
+
+no_args:
+        ; Ejecucion sin argumentos
+        ; Mostrar
+        JMP fin
+
+fin:
         MOV AX, 4C00H ; Fin del programa
         INT 21H
 
